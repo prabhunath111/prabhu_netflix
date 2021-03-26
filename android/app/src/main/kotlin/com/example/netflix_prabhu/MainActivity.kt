5199fc9aebd0b8bc93e9ad5f67db1cade71cc39f
Prabhu Nath Tiwary <prabhunt@micelio.local>
2021-03-26 11:27:58 +0530

    home page implemented

---
+++ android/app/src/main/kotlin/com/example/netflix_prabhu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.netflix_prabhu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_prabhu/json/root_app_json.dart';
+import 'package:netflix_prabhu/pages/home_page.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: false,
+      bottomNavigationBar: getFooter(),
+      body: getBody(),
+    );
+  }
+  Widget getFooter() {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(color: Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(items.length, (index) {
+                return GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      activeTab = index;
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Icon(items[index]['icon'], color: Colors.white),
+                      SizedBox(height: 5),
+                      Text(
+                        items[index]['text'],
+                        style: TextStyle(color: Colors.white, fontSize: 10),
+                      )
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: activeTab,
+      children: [
+        HomePage(),
+        Center(
+          child: Text(
+            'Coming Soon',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Search',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Downloads',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
